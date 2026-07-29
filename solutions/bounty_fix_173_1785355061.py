aea3c6af0df1ab4bf4601765f9fd7da5043b165e
Kette Pryce <johnsmusicbox@gmail.com>
2026-07-29 15:58:49 -0400

    fix(173): Resources page > design system icon button has no label

---
+++ solutions/bounty_fix_173_1785355061.py
@@ -0,0 +1,6 @@
+<!-- Markup example on the resources page -->
+<button class="btn btn-icon">
+  <svg class="icon" viewBox="0 0 24 24">
+    <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/>
+  </svg>
+</button>
